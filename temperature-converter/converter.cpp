fffa5d62acedbaae8e8230ee1345e8ff9fcee453
Jon Corbin <jon.corbin8@gmail.com>
2019-05-01 20:11:19 -0400

    Complete chalenge

--- temperature-converter/converter.cpp
+++ temperature-converter/converter.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
-int main () {
-    int LOWER_BOUND = 0;
-    int UPPER_BOUND = 50000;
+// convert celsius to fahrenheit
+double c2f(double c){
+    return ((c * 9)/5 + 32);
+}
 
-    int start_temp = LOWER_BOUND - 1; 
-    int end_temp = UPPER_BOUND + 1;
+// print table as specified in the challenge
+void printTable(double start, double end, double step){
+    cout << "Celsius\t\tFahrenheit" << endl << "-------\t\t----------" << endl;
+    for (double i = start; i <= end; i+=step){
+        cout << fixed;
+        cout << setprecision(7);
+        cout << i << "\t" << c2f(i) << endl;
+    }
+}
+
+int main () {
+    const int LOWER_BOUND = 0;
+    const int UPPER_BOUND = 50000;
+
+    double start_temp = LOWER_BOUND - 1; 
+    double end_temp = UPPER_BOUND + 1;
 
     // Get Start Temp
     while(start_temp < LOWER_BOUND){
@@ -22,16 +38,14 @@ int main () {
     }
     
     // Get Step Size
-    int step_size;
+    double step_size;
     do {
         cout << "Enter a step size (Must be smaller than " << end_temp - start_temp << ")" << endl;
         cin >> step_size;
     } while(step_size > (end_temp - start_temp));
 
-    // Print Table
-    for (int i = start_temp; i <= end_temp; i+=step_size){
-        cout << i << endl;
-    }
+    printTable(start_temp, end_temp, step_size);
+    
 
     return 0;
 }
